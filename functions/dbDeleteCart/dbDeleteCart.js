16af076103b4fd52e490ccba93cb76f5d6b6af28
pedro <Pecarvajal246@gmail.com>
2022-07-05 18:03:44 -0400

    feat: delete items from users cart

--- functions/dbDeleteCart/dbDeleteCart.js
+++ functions/dbDeleteCart/dbDeleteCart.js
@@ -5,11 +5,16 @@ const handler = async (event) => {
   let { httpMethod: method } = event;
   let client = await connectDB();
   const colUsers = client.db().collection("users");
+  let r;
   if (method == "DELETE") {
     try {
-      const { userId } = JSON.parse(event.body);
-      let r = await colUsers.updateOne({ userId }, { $unset: { items: "" } });
-      console.log(r);
+      const { userId, items } = JSON.parse(event.body);
+      if (items) {
+        console.log(items);
+        r = await colUsers.updateOne({ userId }, { $pullAll: { items } });
+        return output(r);
+      }
+      r = await colUsers.updateOne({ userId }, { $unset: { items: "" } });
       return output(r);
     } catch (error) {
       console.log(error);
